41266fca9d0c55a2e9d213fd8dd7109b4a228a8b
bluurr <chris@bluurr.com>
2019-08-21 18:21:02 +0100

    Added wait condition to aws-upsert script for stack completion.

--- cicd/aws/common/aws-cf-upsert.sh
+++ cicd/aws/common/aws-cf-upsert.sh
@@ -9,6 +9,9 @@ stackStatus=$(aws cloudformation describe-stacks --stack-name ${stackName})
 if [[ ${stackStatus} ]];
 then
    aws cloudformation update-stack --template-body file://${templateFileLocation} --stack-name ${stackName} ${parameters[@]}
+   aws cloudformation wait stack-update-complete --stack-name ${stackName}
 else
     aws cloudformation create-stack --template-body file://${templateFileLocation} --stack-name ${stackName} ${parameters[@]}
+    aws cloudformation wait stack-create-complete --stack-name ${stackName}
 fi
+
