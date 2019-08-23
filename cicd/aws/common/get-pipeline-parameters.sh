0cd3f0e15b4e5663d1c9538b77c182aea1b71d8c
bluurr <chris@bluurr.com>
2019-08-24 00:21:50 +0100

    Minor change to get get-pipeline-parameters.sh for correctly loading the variable names on linux agent.

--- cicd/aws/common/get-pipeline-parameters.sh
+++ cicd/aws/common/get-pipeline-parameters.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-
 ## compgen https://unix.stackexchange.com/questions/151118/understand-compgen-builtin-command
 compgen -v | while read -r PARAM_NAME; do
     ## Filter to variables which are support.
-    if [[ ${PARAM_NAME} == 'PARAMETER_'* ]]
+    if [[ "${PARAM_NAME}" == "PARAMETER_"* ]]
     then
         echo "${PARAM_NAME}"
     fi
